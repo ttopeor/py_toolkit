4e55dc7c9bef0f5eac0a59d4b9bf375a1e033e0b
yue <ttopeor@udel.edu>
2025-11-14 16:44:50 -0800

    chage controlloer

--- admitance_controller_6d.py
+++ admitance_controller_6d.py
@@ -2,22 +2,20 @@ import numpy as np
 from typing import Sequence
 from py_toolkit.math_tools import parent_to_child
 
+
 class AdmittanceController6D:
     def __init__(
         self,
-        M: Sequence[float],
         B: Sequence[float],
         init_p: Sequence[float],
     ):
-        assert len(M) == len(B) == 6, "Lengths of M and B must be 6"
+        assert len(B) == 6, "Lengths of B must be 6"
         # Convert to NumPy arrays for vectorised maths
-        self.M = np.array(M, dtype=float)
         self.B = np.array(B, dtype=float)
         self.p = np.copy(np.array(init_p, dtype=float))
 
         self.F_ext_d = np.zeros(6, dtype=float)
-        self._last_v = np.zeros(6, dtype=float)
-
+        # self._last_v = np.zeros(6, dtype=float)
 
     def set_desired_force(self, F_ext_d: Sequence[float]) -> None:
         assert len(F_ext_d) == 6, "Desired force must have length 6"
@@ -28,11 +26,14 @@ class AdmittanceController6D:
         e_f = self.F_ext_d - F_ext
 
         # M * a + B * v = e_f  ->  a = (e_f - B * v) / M
-        a = (e_f - self.B * self._last_v) / self.M
-        v = self._last_v + a * dt
+        # a = (e_f - self.B * self._last_v) / self.M
+        # v = self._last_v + a * dt
+
+        v = e_f / self.B
+
         delta_p = v * dt
-        
+
         self.p = parent_to_child(self.p, delta_p)
 
-        self._last_v = v
+        # self._last_v = v
         return self.p
--- impedance_controller_6d.py
+++ impedance_controller_6d.py
@@ -2,12 +2,13 @@ import numpy as np
 from typing import Sequence
 from py_toolkit.math_tools import calculate_delta_position
 
+
 class ImpedanceController6D:
     def __init__(self, B: Sequence[float], K: Sequence[float], init_p: Sequence[float]):
         assert len(B) == len(K) == 6, "B, K must have length 6"
         self.B = np.array(B, dtype=float)
         self.K = np.array(K, dtype=float)
-        
+
         self.p_d = np.copy(np.array(init_p, dtype=float))
         self._last_p = np.copy(np.array(init_p, dtype=float))
 
@@ -19,10 +20,9 @@ class ImpedanceController6D:
         p = np.array(p, dtype=float)
 
         e_p = calculate_delta_position(p, self.p_d)
-        e_v = calculate_delta_position(p, self._last_p) / dt 
+        e_v = calculate_delta_position(p, self._last_p) / dt
 
-        F_cmd = self.K * e_p + self.B * e_v 
-
+        F_cmd = self.K * e_p + self.B * e_v
         self._last_p = p
 
         return F_cmd
