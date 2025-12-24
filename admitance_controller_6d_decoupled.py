04c25251f8446a14e3f3b54bc6b32608bdf1959e
yue <ttopeor@udel.edu>
2025-12-23 19:10:42 -0800

    update

---
+++ admitance_controller_6d_decoupled.py
@@ -0,0 +1,34 @@
+import numpy as np
+from typing import Sequence
+from py_toolkit.math_tools import parent_to_child
+
+
+class AdmittanceController6D:
+    def __init__(
+        self,
+        B: Sequence[float],
+        init_p: Sequence[float],
+    ):
+        assert len(B) == 6, "Lengths of B must be 6"
+        # Convert to NumPy arrays for vectorised maths
+        self.B = np.array(B, dtype=float)
+        self.p = np.copy(np.array(init_p, dtype=float))
+
+        self.F_ext_d = np.zeros(6, dtype=float)
+        # self._last_v = np.zeros(6, dtype=float)
+
+    def set_desired_force(self, F_ext_d: Sequence[float]) -> None:
+        assert len(F_ext_d) == 6, "Desired force must have length 6"
+        self.F_ext_d = np.array(F_ext_d, dtype=float)
+
+    def update(self, F_ext: Sequence[float], dt: float) -> np.ndarray:
+        F_ext = np.array(F_ext, dtype=float)
+        e_f = F_ext - self.F_ext_d
+
+        v = e_f / self.B
+
+        delta_p = v * dt
+
+        self.p = parent_to_child(self.p, delta_p)
+
+        return self.p
---
+++ impedance_controller_6d_decoupled.py
@@ -0,0 +1,41 @@
+import numpy as np
+from typing import Sequence
+from py_toolkit.math_tools import calculate_delta_position, print_array
+
+
+class ImpedanceController6D:
+    def __init__(
+        self,
+        B: Sequence[float],
+        K: Sequence[float],
+        init_p: Sequence[float],
+        vel_tau: float = 0.03,  # larger smoother
+    ):
+        assert len(B) == len(K) == 6, "B, K must have length 6"
+        self.B = np.array(B, dtype=float)
+        self.K = np.array(K, dtype=float)
+
+        self.p_d = np.copy(np.array(init_p, dtype=float))
+        self._last_p = np.copy(np.array(init_p, dtype=float))
+
+        self.vel_tau = float(vel_tau)
+        self._e_v_est = np.zeros(6, dtype=float)
+
+    def set_desired_position(self, p_d: Sequence[float]) -> None:
+        assert len(p_d) == 6, "Desired position must have length 6"
+        self.p_d = np.array(p_d, dtype=float)
+
+    def update(self, p: Sequence[float], dt: float) -> np.ndarray:
+        p = np.array(p, dtype=float)
+
+        e_p = calculate_delta_position(self.p_d, p)
+        e_v_raw = calculate_delta_position(self._last_p, p) / dt
+
+        alpha = dt / (self.vel_tau + dt)
+        # e_v_est(k) = e_v_est(k-1) + alpha * (raw - e_v_est(k-1))
+        self._e_v_est = self._e_v_est + alpha * (e_v_raw - self._e_v_est)
+        e_v = self._e_v_est
+
+        F_cmd = self.K * e_p + self.B * e_v
+        self._last_p = p
+        return F_cmd
